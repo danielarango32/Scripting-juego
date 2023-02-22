c8ab3c175261a493094c60b279c6f327ef9c943b
Felipeupb <felipe.aguilar@upb.edu.co>
2023-02-22 00:40:27 -0500

    movePlatform

--- Assets/MovePlatform.cs
+++ Assets/MovePlatform.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
+    
+    public GameObject ObjetoAmover;
+    
+    public Transform StartPoint;
+    
+    public Transform EndPoint;
+    
+    public float Velocidad;
+    
+    private Vector3 MoverHacia;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoverHacia = EndPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ObjetoAmover.transform.position = Vector3.MoveTowards (ObjetoAmover.transform.position, MoverHacia, Velocidad * Time.deltaTime);
+        if(ObjetoAmover.transform.position == EndPoint.position)
+        {
+            Noverhacia - StertPoint.position;
+        }
+        if (ObjetoAmover. transform.position == Stertpoint.position)
+        {
+            Moverhacia - Endpoint.position;
+        }
     }
 }
