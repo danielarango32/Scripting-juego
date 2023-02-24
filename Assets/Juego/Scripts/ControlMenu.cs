b0744a943eab5539c51b585af3bf6ada9ec9e224
Daniel Arango <daniel.arangoe@upb.edu.co>
2023-02-24 02:55:08 -0500

    Navegación Código en progreso

---
+++ Assets/Juego/Scripts/ControlMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenu : MonoBehaviour
+{
+    public void OnPressJugar()
+    {
+        SceneManager.LoadScene("Juego", LoadSceneMode.Single);
+    }
+    public void OnPressInstrucciones()
+    {
+        SceneManager.LoadScene("Instrucciones", LoadSceneMode.Single);
+    }
+    public void OnPressCreditos()
+    {
+        SceneManager.LoadScene("Creditos", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Juego/Scripts/Volver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volver : MonoBehaviour
+{
+public void OnPressVolver()
+    {
+        Debug.Log("Volver");
+    }
+}
